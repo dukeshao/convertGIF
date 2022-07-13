0702a149f7ec56f16f013f66409d401f88473d42
shaozhonghua_ext <zhonghua.shao@zznode.com>
2022-07-13 09:48:04 +0800

    添加备注

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
 
 const ffmpeg = createFFmpeg({ log: true });
 
+// 要转换的视频
 const fileName = "4D7136CB-B63B-4862-90FC-F374E6B73FB5.MOV";
+// 转换后的 GIF 图名称
 const resultName = "demo.gif";
 
 (async () => {
